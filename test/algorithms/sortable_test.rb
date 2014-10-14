24e222c3134a0f071a05f50d29e36de30b6b5dfd
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-10-14 15:37:01 -0700

    add test files

---
+++ test/algorithms/sortable_test.rb
@@ -0,0 +1,30 @@
+require_relative '../../lib/algorithms/sortable'
+require 'minitest/autorun'
+
+class SortableTest < MiniTest::Test
+
+  def test_bubble_sort
+    ar = [1,3,53,5,21,6]
+    ar =Algorithms::Sortable.bubble_sort(ar)
+    assert_equal([1,3,5,6,21,53], ar)
+  end
+
+  def test_selection_sort
+    ar = [1,3,53,5,21,6]
+    ar =Algorithms::Sortable.selection_sort(ar)
+    assert_equal([1,3,5,6,21,53], ar)
+  end
+
+  def test_insertion_sort
+    ar = [1,3,53,5,21,6]
+    ar =Algorithms::Sortable.insertion_sort(ar)
+    assert_equal([1,3,5,6,21,53], ar)
+  end
+
+  def test_odd_even_sort
+    ar = [1,3,53,5,21,6]
+    ar =Algorithms::Sortable.odd_even_sort(ar)
+    assert_equal([1,3,5,6,21,53], ar)
+  end
+  
+end
