ec4c61297bceebe5a62ae53aaff167d10920abf9
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-10-22 05:50:56 -0700

    binary search tree insert method

--- lib/data_structures/binary_search_tree.rb
+++ lib/data_structures/binary_search_tree.rb
@@ -1,4 +1,46 @@
 module DataStructures
   class Node
+
+    attr_reader :key, :value
+    attr_accessor :right_child, :left_child
+    def initialize(key, value)
+      @key = key
+      @value = value
+    end
+
+  end
+
+  class Tree
+
+    attr_reader :root
+    def initialize
+      @root = nil
+    end
+
+    def insert(key, value)
+      node = Node.new(key, value)
+      if @root
+        current = @root
+        parent = nil
+        loop do
+          parent = current
+          if key < current.key
+            current = current.left_child
+            unless current
+              parent.left_child = newNode
+              break
+            end
+          else
+            current = current.right_child
+            unless currnet
+              parent.right_child = node
+              break
+            end
+          end
+        end
+      else
+        @root = node
+      end
+    end
   end
 end
--- test/data_structures/binary_search_tree_test.rb
+++ test/data_structures/binary_search_tree_test.rb
@@ -3,14 +3,27 @@ require 'minitest/autorun'
 
 class NodeTest < MiniTest::Test
   
-  def test_empty_node
-    n = DataStructures::Node.new
-    assert_equal(nil, n.value)
-    assert_equal(nil, n.key)
+  def test_new_node
+    n = DataStructures::Node.new(0, 'hello')
+    assert_equal('hello', n.value)
+    assert_equal(0, n.key)
+  end
+
+  def test_node_right_child
+    n = DataStructures::Node.new(0, 'hello')
+    rn = DataStructures::Node.new(1, 'bye')
+    n.right_child = rn
+    assert_equal(rn, n.right_child)
   end
 
 end
 
-class BinarySearchTreeTest < MiniTest::Test
+class TreeTest < MiniTest::Test
+
+  def test_inserting
+    t = DataStructures::Tree.new
+    t.insert(10, 'hello')
+    assert_equal('hello', t.root.value)
+  end
 
 end
