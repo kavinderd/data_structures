0d20fe5a59cc1068bd23db8ad0479d953d25828c
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-10-29 16:18:55 -0700

    red black tree finished...still need to understand this thing a little better

--- lib/data_structures/red_black_tree.rb
+++ lib/data_structures/red_black_tree.rb
@@ -1,12 +1,12 @@
-require_relative 'binary_search_tree'
 module DataStructures
 
-  class RedBlackNode < Node
+  class RedBlackNode 
 
-    attr_reader :color
-    def initialize(key, value, color=:red)
+    attr_reader :key
+    attr_accessor :right_child, :left_child, :parent, :color
+    def initialize(key,color=:red)
+      @key = key
       @color = color
-      super(key, value)
     end
 
     def red?
@@ -17,79 +17,132 @@ module DataStructures
       @color == :black
     end
 
-    def turn_black!
-      @color = :black
-    end
-
-    def turn_red!
-      @color = :red
-    end
-
-    def red_children?
-      left_child.red? && right_child.red?
-    end
-
-    def black_children?
-      left_child.black? && right_child.black?
-    end
-
     def flip!
-      red? ? @color = :black : @color = red
+      @color = red? ? :black : :red
     end
 
-  end
-
-  class RedBlackTree < Tree
-
-    def insert(key, value)
-      node = RedBlackNode.new(key, value)
-      if @root
-        current = @root
-        parent = nil
-        grand = nil
-        loop do
-          grand = parent
-          parent = current
-          color_flip if needs_flip?(parent) && not_root(parent) # If Parent is Black and Children are Red
-          if key < current.key
-            current = current.left_child
-            unless current
-              parent.left_child = node
-              break
-            end
-          else
-            current = current.right_child
-            unless current
-              parent.right_child = node
-              break
-            end
-          end
-        end
+    def set_child(dir, node)
+      if dir == :right
+        @right_child = node
       else
-        node.turn_black!
-        @root = node
+        @left_child = node
       end
     end
+
+    def get_child(dir)
+      self.send("#{dir.to_s}_child")
+    end
+
+    def grandparent
+      parent.parent if parent
+    end
+    
+    def right_uncle 
+      if grandparent
+        grandparent.right_child unless grandparent.right_child == parent
+      end
+    end
+
+    def left_uncle
+      if grandparent
+        grandparent.left_child unless grandparent.left_child == parent
+      end
+    end
+
+    def is_right_child?
+      return nil unless @parent 
+      @parent.right_child == self
+    end
+
+    def is_left_child?
+      return nil unless @parent
+      @parent.left_child == self
+    end
+
+    def sidedness
+      is_left_child? ? :left : :right
+    end
+
   end
 
-  private 
+  class RedBlackTree
 
-  def needs_flip?(node)
-    node.black? && node.red_children?
+    attr_reader :root
+    def initialize
+      
+    end
+
+    def insert(key)
+      node = RedBlackNode.new(key)
+      rb_insert(node)
+    end
+
+    def rb_insert(node)
+      unless @root
+        @root = node
+        node.flip!
+        return
+      end
+      tree_insert(node)
+      until node == @root || node.parent.color == :black
+        uncle = node.parent.is_left_child? ? node.right_uncle : node.left_uncle
+        node = cases(node, uncle)
+        @root.color = :black
+      end
+    end
+
+    def tree_insert(node, root=@root)
+      dir = node.key < root.key ? :left : :right
+      if root.get_child(dir)
+        tree_insert(node, root.get_child(dir))
+      else
+        root.set_child(dir, node)
+        node.parent = root
+      end
+    end
+
+    def cases(node, uncle)
+      if uncle.nil? || uncle.color == :black
+        black_uncle(node, uncle)
+      else
+        recolor(node, uncle)
+      end
+    end
+
+    def black_uncle(node, uncle)
+      if node.sidedness != node.parent.sidedness
+        node = node.parent
+        rotate(node, node.sidedness)
+      end
+      node.parent.color = :black
+      node.grandparent.color = :red
+      opp_dir = node.is_left_child? ? :right : :left
+      rotate(node.grandparent, opp_dir)
+      node.grandparent
+    end
+
+    def recolor(node, uncle)
+      node.parent.color = :black
+      uncle.color = :black
+      node.grandparent.color = :red
+      node = node.grandparent
+      node
+    end
+
+    def rotate(parent, dir1)
+      dir2 = dir1 == :left ? :right : :left
+      child = parent.get_child(dir2)
+      parent.set_child(dir2, child.get_child(dir1))
+      child.get_child(dir1).parent = parent unless child.get_child(dir1).nil?
+      if parent == @root
+        @root = child
+      else
+        child.parent = parent.parent
+        parent.parent.set_child(parent.sidedness, child)
+      end
+      parent.parent = child
+      child.set_child(dir1, parent)
+    end
+    
   end
-
-  def not_root(node)
-    !root?(node)
-  end
-
-  def root?(node)
-    node == @root
-  end
-
-  def color_flip(node)
-    node.flip!
-    node.left_child.flip!
-    node.right_child.flip!
-  end
-
 end
--- test/data_structures/red_black_tree_test.rb
+++ test/data_structures/red_black_tree_test.rb
@@ -1,47 +1,126 @@
 require_relative '../../lib/data_structures/red_black_tree'
 require 'minitest/autorun'
+require 'pry'
+require 'pry-debugger'
 
 class RedBlackNodeTest < MiniTest::Test
 
-  def test_color_field
-    node = DataStructures::RedBlackNode.new(5, 'value')
+  def test_red_by_default
+    node = DataStructures::RedBlackNode.new(1)
     assert(node.red?)
-    refute(false, node.black?)
   end
 
-  def test_changing_color
-    node = DataStructures::RedBlackNode.new(5, 'value')
-    node.turn_black!
-    assert(node.black?)
-    node.turn_red!
-    assert(node.red?)
+  def test_nil_family_by_default
+    node = DataStructures::RedBlackNode.new(1)
+    refute(node.parent)
+    refute(node.left_child)
+    refute(node.right_child)
   end
+
+  def test_getting_child_with_dir
+    node = DataStructures::RedBlackNode.new(1)
+    rchild = DataStructures::RedBlackNode.new(3)
+    lchild = DataStructures::RedBlackNode.new(5)
+    node.set_child(:left, lchild)
+    node.set_child(:right, rchild)
+    assert_equal(node.get_child(:right), node.right_child)
+    assert_equal(node.get_child(:left), node.left_child)
+  end
+
+
+  def test_parent_link
+    child = DataStructures::RedBlackNode.new(1)
+    parent = DataStructures::RedBlackNode.new(5)
+    child.parent = parent
+    assert_equal(5, child.parent.key)
+  end
+
+  def test_grandparent_link
+    child = DataStructures::RedBlackNode.new(1)
+    parent = DataStructures::RedBlackNode.new(5)
+    grandparent = DataStructures::RedBlackNode.new(10)
+    child.parent = parent
+    parent.parent = grandparent
+    assert_equal(10, child.grandparent.key)
+  end
+
+  def test_node_sidedness
+    child = DataStructures::RedBlackNode.new(1)
+    parent = DataStructures::RedBlackNode.new(5)
+    parent.right_child = child
+    child.parent = parent
+    assert(child.is_right_child?)
+    refute(child.is_left_child?)
+  end
+
+  def test_uncle_link
+    child = DataStructures::RedBlackNode.new(1)
+    parent = DataStructures::RedBlackNode.new(5)
+    grandparent = DataStructures::RedBlackNode.new(10)
+    uncle = DataStructures::RedBlackNode.new(15) 
+    grandparent.left_child = parent and parent.parent = grandparent
+    grandparent.right_child = uncle and uncle.parent = grandparent
+    child.parent = parent
+    assert_equal(15, child.right_uncle.key)
+    refute(child.left_uncle)
+  end
+
 
 end
+
 class RedBlackTreeTest < MiniTest::Test
 
-  def test_root_node_is_black
-    rb = DataStructures::RedBlackTree.new
-    rb.insert(50, 'hello')
-    assert(rb.root.black?)
+  def setup
+    @rbtree = DataStructures::RedBlackTree.new
   end
 
-  def test_inserting_two_red_nodes
-    rb = DataStructures::RedBlackTree.new
-    rb.insert(50, 'val1')
-    rb.insert(25, 'val2')
-    rb.insert(75, 'val3')
-    assert(rb.root.right_child.red?)
-    assert(rb.root.left_child.red?)
+  def test_nil_root_at_initialization
+    refute(@rbtree.root) 
   end
 
-  def test_simple_black_height_rule
-    rb = DataStructures::RedBlackTree.new
-    rb.insert(50, 'val1')
-    rb.insert(25, 'val2')
-    rb.insert(75, 'val3')
-    rb.insert(12, 'val4')
-    assert(rb.root.left_child.black?)
-    assert(rb.root.right_child.black?)
+  def test_inserting_root_node
+    @rbtree.insert(45)
+    assert_equal(45, @rbtree.root.key)
+    assert(@rbtree.root.black?)
   end
+
+  def test_insert_red_child_to_black_node
+    @rbtree.insert(50)
+    @rbtree.insert(55)
+    @rbtree.insert(45)
+    assert(@rbtree.root.left_child.red?)
+    assert(@rbtree.root.right_child.red?)
+  end
+
+  def test_balance_when_inserting_red_child_inside_no_uncle
+    @rbtree.insert(1)
+    @rbtree.insert(7)
+    @rbtree.insert(3)
+    assert_equal(3, @rbtree.root.key)
+    assert_equal(1, @rbtree.root.left_child.key)
+    assert_equal(7, @rbtree.root.right_child.key)
+  end
+
+  def test_balance_when_inserting_red_child_outside_no_uncle
+    @rbtree.insert 1
+    @rbtree.insert 7
+    @rbtree.insert 8
+    assert_equal(7, @rbtree.root.key)
+    assert_equal(1, @rbtree.root.left_child.key)
+    assert_equal(8, @rbtree.root.right_child.key)
+  end
+
+  def test_balance_when_inserting_red_child_to_red_parent_opposite_of_uncle
+    @rbtree.insert 3
+    @rbtree.insert 1
+    @rbtree.insert 7
+
+    @rbtree.insert 8
+    assert_equal(3, @rbtree.root.key)
+    assert(@rbtree.root.right_child.black?)
+    assert(@rbtree.root.left_child.black?)
+    assert(@rbtree.root.right_child.right_child.red?)
+  end
+
+ 
 end
