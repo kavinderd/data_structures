d213fbdfb72a8e2a634f14358390ad1ec220a1aa
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-11-03 08:25:25 -0800

    add double hashing algorithm

--- lib/data_structures/hash_table.rb
+++ lib/data_structures/hash_table.rb
@@ -1,66 +1,127 @@
 require 'pry'
 require 'pry-debugger'
 module DataStructures
-  module LinearProbe
-    class DataItem
+  class DataItem
 
-      attr_reader :value
-      def initialize(value)
-        @value = value
-      end
-
-      def key
-        @value
-      end
-
+    attr_reader :value
+    def initialize(value)
+      @value = value
     end
-    class HashTable
 
-      def initialize(size)
-        @size = size
-        @hash = Array.new(size)
-      end
-
-      def insert(value)
-        item = DataItem.new(value)
-        key = hash(item.key)
-        while @hash[key] && key < @size
-          key += 1
-          key = hash(key) 
-        end
-        @hash[key] = item
-      end
-
-      def find(value)
-        key = hash(value)
-        while @hash[key] && key < @size
-          if @hash[key].key == value
-            return @hash[key]
-          end
-          key += 1
-          key = hash(key)
-        end
-      end
-
-      def remove(value)
-        key = hash(value)
-        while @hash[key] && key < @size
-          if @hash[key].key == value
-            @hash.delete_at(key)
-            return nil
-          end
-          key += 1
-          key = hash(key)
-        end
-      end
-
-      private
-
-      def hash(key)
-        key % @size
-      end
-
-
+    def key
+      @value
     end
+
+  end
+  class NullDataItem
+    def key
+      -1
+    end
+  end
+  class HashTable
+    HASH_TYPES = [:linear_probing, :double_hashing]
+
+    def initialize(size, type=:linear_probing)
+      @size = size
+      @hash = Array.new(size)
+      if HASH_TYPES.include?(type)
+        @hash_type = type
+      else
+        raise "Hash type not valid"
+      end
+    end
+
+    def insert(value)
+      send("#{@hash_type}_insert", value)
+    end
+
+    def find(value)
+      send("#{@hash_type}_find", value)
+    end
+
+    def remove(value)
+      send("#{@hash_type}_remove", value)
+    end
+
+    private 
+    
+    def double_hashing_insert(value)
+      item = DataItem.new(value)
+      key = hash(item.key)
+      step = double_hash(key)
+      while @hash[key] && key < @size
+        key += step
+        key = hash(key)
+      end
+      @hash[key] = item
+    end
+
+    def double_hashing_find(value)
+      key = hash(value)
+      step = double_hash(key)
+      while @hash[key] && key < @size
+        if @hash[key].key == value
+          return @hash[key]
+        end
+        key += step
+        key = hash(key)
+      end
+    end
+
+    def double_hashing_remove(value)
+      key = hash(value)
+      step = double_hash(value)
+      while @hash[key] && key < @size
+        if @hash[key].key == value
+          @hash[key] = NullDataItem.new
+          return nil
+        end
+        key += 1
+        key = hash(key)
+      end
+    end
+
+    def linear_probing_insert(value)
+      item = DataItem.new(value)
+      key = hash(item.key)
+      while @hash[key] && key < @size
+        key += 1
+        key = hash(key) 
+      end
+      @hash[key] = item
+    end
+
+    def linear_probing_find(value)
+      key = hash(value)
+      while @hash[key] && key < @size
+        if @hash[key].key == value
+          return @hash[key]
+        end
+        key += 1
+        key = hash(key)
+      end
+    end
+
+    def linear_probing_remove(value)
+      key = hash(value)
+      while @hash[key] && key < @size
+        if @hash[key].key == value
+          @hash[key] = NullDataItem.new
+          return nil
+        end
+        key += 1
+        key = hash(key)
+      end
+    end
+
+    def hash(key)
+      key % @size
+    end
+
+    def double_hash(key)
+      5 - ( key % 5 )
+    end
+
+
   end
 end
--- test/data_structures/hash_table_test.rb
+++ test/data_structures/hash_table_test.rb
@@ -1,25 +1,26 @@
 require_relative "../../lib/data_structures/hash_table"
 require "minitest/autorun"
 
-# Linear Probe
+class HashTableTest < MiniTest::Test
 
-class LinearProbeTest < MiniTest::Test
-
+  def setup
+    @type = DataStructures::HashTable::HASH_TYPES.sample
+  end
   def test_hashing_an_inserted_item
-    h = DataStructures::LinearProbe::HashTable.new(100)
+    h = DataStructures::HashTable.new(100, @type)
     h.insert(453)
     assert_equal(453, h.find(453).value)
   end
 
   def test_hashing_a_duplicate_hash
-    h = DataStructures::LinearProbe::HashTable.new(100)
+    h = DataStructures::HashTable.new(100, @type)
     h.insert(453)
     h.insert(4053)
     assert_equal(4053, h.find(4053).value)
   end
 
   def test_removing_an_item
-    h = DataStructures::LinearProbe::HashTable.new(10)
+    h = DataStructures::HashTable.new(10,@type)
     h.insert(11)
     h.insert(1)
     h.remove(11)
@@ -28,3 +29,4 @@ class LinearProbeTest < MiniTest::Test
   end
 
 end
+
